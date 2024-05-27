317b513197d9a3d094468a50b05d6fda62454b2f
Prajin Khatiwada <prajinkhatiwada@gmail.com>
2024-05-28 00:08:51 +0545

    registered our models in admin site

--- mychat/chat/admin.py
+++ mychat/chat/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Room , Message
 
-# Register your models here.
+# Registering mychat models.
+admin.site.register(Room)
+admin.site.register(Message)
